eb5a45624c6ad66fa27306adb2973e144af4ce52
Matthew Wood <mattwood2855@gmail.com>
2023-12-18 10:10:37 -0800

    Bootstrap

---
+++ HARbinger.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HARbinger.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ HARbinger.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HARbinger.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
