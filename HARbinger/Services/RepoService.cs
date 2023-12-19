511257ffcb4f3cc7a9dbb74089c2c2f10d6fd11a
Matthew Wood <mattwood2855@gmail.com>
2023-12-19 12:46:03 -0800

    page updates

---
+++ HARbinger/Services/RepoService.cs
@@ -0,0 +1,46 @@
+﻿using LibGit2Sharp;
+
+namespace HARbinger.Services
+{
+    public class RepoData
+    {
+        public string Branch { get; }
+        public string Name { get; }
+
+        public RepoData(Repository repo)
+        {
+            Branch = repo.Head.FriendlyName;
+            Name = repo.Info.WorkingDirectory;
+        }
+    }
+
+    public static class RepoService
+    {
+        public static Repository Repository { get; private set; }
+
+        public static RepoData? GetRepoDataForPath(string path)
+        {
+            if(string.IsNullOrWhiteSpace(path))
+            {
+                return null;
+            }
+
+            if(!Repository.IsValid(path))
+            {
+                return null;
+            }
+
+            try
+            {
+                Repository = new Repository(path);
+                var repoData = new RepoData(Repository);
+                return repoData;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
